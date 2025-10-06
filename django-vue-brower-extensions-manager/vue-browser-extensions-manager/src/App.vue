68d892635d16983e7d151c1e4ba21317e77054c2
Therealajahn <therealajahn@gmail.com>
2025-10-06 18:48:34 -0500

    main

---
+++ django-vue-brower-extensions-manager/vue-browser-extensions-manager/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+	import { ref, onMounted } from 'vue';
+
+	const count = ref(0);
+	function increment(){
+		count++;
+	};
+	onMounted(() => {
+		console.log(`Initiial count:${count}`)
+	});
+</script>
+
+<template>
+	<button @click="count++">Count is: {{ count }}</button>
+</template>
+
+<style scoped></style>
